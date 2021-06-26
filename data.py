c2e97b0c6e92ebae268b64054eea55dca6d7db5f
Buddhika Chathuranga <vmbckscc456@gmail.com>
2021-06-27 02:22:12 +0530

    data added

---
+++ data.py
@@ -0,0 +1,32 @@
+inventory = [
+    {
+        'Item Code': 1,
+        'Item Name': 'iPhone',
+        'Price': 130000,
+        'In Stock': 4
+    },
+    {
+        'Item Code': 2,
+        'Item Name': 'Air Dots 2',
+        'Price': 4500,
+        'In Stock': 4
+    },
+    {
+        'Item Code': 3,
+        'Item Name': 'mi Band',
+        'Price': 9000,
+        'In Stock': 4
+    },
+    {
+        'Item Code': 4,
+        'Item Name': 'Jabra Headset',
+        'Price': 10000,
+        'In Stock': 4
+    }
+]
+
+shoppingCart = [
+
+]
+
+
